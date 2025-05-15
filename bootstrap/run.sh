2b4cf06fabfa3d018b20877b72102c29019885a3
Ryan Macdonald <6629959+iamryanmacdonald@users.noreply.github.com>
2025-05-15 16:22:33 +1000

    modified external gateway api to allow http across namespaces

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -2,7 +2,7 @@
 set -eoux pipefail
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.17.0/config/crd/standard/dnsendpoint.yaml
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
 LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
 curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/stripped-down-crds.yaml | kubectl apply --server-side --filename -
 
