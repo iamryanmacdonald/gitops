3d06bc726993527a40e6173acc0307f1dec4ebe4
Ryan Macdonald <6629959+iamryanmacdonald@users.noreply.github.com>
2025-06-04 20:52:52 +1000

    fixed bootstrap process

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
-LATEST=$(curl -s https://api.github.com/repos/kubernetes-sigs/external-dns/releases/latest | jq -cr .tag_name)
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/${LATEST}/config/crd/standard/dnsendpoint.yaml
-LATEST=$(curl -s https://api.github.com/repos/kubernetes-sigs/gateway-api/releases/latest | jq -cr .tag_name)
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/${LATEST}/experimental-install.yaml
-LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
-curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/stripped-down-crds.yaml | kubectl apply --server-side --filename -
-
-sops --decrypt "${ROOT_DIR}/kubernetes/clusters/${CLUSTER}/secrets/sops-age.sops.yaml" | kubectl apply -f -
+kubectl create namespace security
+sops --decrypt "${ROOT_DIR}/bootstrap/bitwarden.sops.yaml" | kubectl apply -f -
+sops --decrypt "${ROOT_DIR}/bootstrap/cluster-secrets.sops.yaml" | kubectl apply -f -
 
 helmfile --file "${ROOT_DIR}/bootstrap/helmfile.yaml" apply --hide-notes --skip-diff-on-install --suppress-diff --suppress-secrets
