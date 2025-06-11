77bebe4e32c304b58dba21f94fd51037bf26bfe1
Ryan Macdonald <6629959+iamryanmacdonald@users.noreply.github.com>
2025-06-11 21:55:01 +1000

    update terraform backend configs to allow terraform workflow to minimize required variables

--- terraform/oci/_terraform.tf
+++ terraform/oci/_terraform.tf
@@ -1,5 +1,15 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "oci/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
   required_providers {
     oci = {
       source  = "oracle/oci"
